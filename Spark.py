deaaaf26d2eebf16c729dd62d5714716750516d2
krishna <31917114+kmaddikara@users.noreply.github.com>
2017-10-03 12:46:06 -0700

    Test
    Spark-Test

---
+++ Spark.py
@@ -0,0 +1,15 @@
+
+# coding: utf-8
+
+# In[27]:
+
+from pyspark.conf import SparkConf
+from contextlib import contextmanager
+from pyspark import SparkContext
+from pyspark import SparkConf
+
+
+# In[34]:
+
+sc.range(100).count()
+
